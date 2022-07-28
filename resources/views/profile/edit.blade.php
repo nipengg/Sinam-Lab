ba9ff4f5d046d804b8f2e7798b40fb6718b7c1cc
nipengg <nevillecorneliustj@gmail.com>
2022-07-28 19:59:25 +0700

    password validation

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -30,7 +30,7 @@
                                             </div>
                                         </div>
                                         <div class="col-xs-12 col-sm-12 col-md-12 text-center">
-                                            <button type="submit" class="btn btn-success">Submit</button>
+                                            <button type="submit" class="btn btn-success">Update</button>
                                         </div>
                                     </div>
                                 </form>
--- resources/views/profile/password.blade.php
+++ resources/views/profile/password.blade.php
@@ -1,5 +1,23 @@
 @extends('layouts.sidebar')
 
+<script type="text/javascript">
+    function check()
+    {
+        var old = document.getElementById("old").value;
+        var newPass = document.getElementById("new").value;
+        var con = document.getElementById("confirm").value;
+
+        if (newPass == con && (newPass != '' || con != ''))
+        {
+            document.getElementById("Button").disabled = false;
+        }
+        else
+        {
+            document.getElementById("Button").disabled = true;
+        }
+    }
+</script>
+
 @section('content')
     <div class="main">
         <div class="main-content">
@@ -17,26 +35,26 @@
                                         <div class="col-xs-12 col-sm-12 col-md-12">
                                             <div class="form-group">
                                                 <strong>Old Password:</strong>
-                                                <input type="password" name="old" class="form-control"
-                                                    placeholder="Old Password" required>
+                                                <input type="password" id="old" name="old" class="form-control"
+                                                    placeholder="Old Password" required onkeyup="check()">
                                             </div>
                                         </div>
                                         <div class="col-xs-12 col-sm-12 col-md-12">
                                             <div class="form-group">
                                                 <strong>New Password:</strong>
-                                                <input type="password" name="new" class="form-control"
-                                                    placeholder="New Password" required>
+                                                <input type="password" id="new" name="new" class="form-control"
+                                                    placeholder="New Password" required onkeyup="check()">
                                             </div>
                                         </div>
                                         <div class="col-xs-12 col-sm-12 col-md-12">
                                             <div class="form-group">
                                                 <strong>Confirm Password:</strong>
-                                                <input type="password" name="confirm" class="form-control"
-                                                    placeholder="Confirm Password" required>
+                                                <input type="password" id="confirm" name="confirm" class="form-control"
+                                                    placeholder="Confirm Password" required onkeyup="check()">
                                             </div>
                                         </div>
                                         <div class="col-xs-12 col-sm-12 col-md-12 text-center">
-                                            <button type="submit" class="btn btn-success">Update Password</button>
+                                            <button id="Button" type="submit" class="btn btn-success" disabled>Update Password</button>
                                         </div>
                                     </div>
                                 </form>
